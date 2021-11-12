57177c055507050dd67261ca5515a3a6e5cfddcc
Karthik Balakrishnan <bkarthik@berkeley.edu>
2021-11-12 11:46:33 -0800

    data scraper built

--- data_collection.py
+++ data_collection.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 import json
 import time
 from datetime import datetime
@@ -12,9 +13,9 @@ OAUTH_TOKEN = credentials["OAUTH_TOKEN"]
 
 f.close()
 
-streamers = ["xqcow", "hasanabi", "ludwig", "nl_kripp", "lirik", "shroud", "mizkif", "gmhikaru"]
+streamers = ["xqcow", "hasanabi", "ludwig", "nl_kripp", "lirik", "shroud", "mizkif", "gmhikaru", "healthygamer_gg"]
 
-def is_user_live(username):
+def curr_info(username):
     endpoint = 'https://api.twitch.tv/helix/streams?'
     my_headers = {
         'Client-ID': CLIENT_ID,
@@ -25,8 +26,11 @@ def is_user_live(username):
     data = response.json()['data']
     if len(data) == 0:
         return False
+    
+    return data[0]
 
-    return data[0]['type'] == 'live'
+def is_user_live(curr_info_json):
+    return curr_info_json
 
 def get_viewers(name):
     url = f"http://tmi.twitch.tv/group/user/{name.lower()}/chatters"
@@ -37,18 +41,21 @@ def get_viewers(name):
     
     return viewers
 
-def update_data(streamer):
-    with f as open(streamer+".csv", "a"):
-        pass
+def update_data(streamer, curr_info_json):
+    with open("data/all_streamer_data.csv", "a") as database:
+        writer = csv.writer(database)
+        viewers = get_viewers(streamer)
+        writer.writerow([streamer, datetime.now(), curr_info_json['started_at'], curr_info_json['game_name'], curr_info_json['viewer_count'], viewers])
 
 def main():
 
     while True:
         for streamer in streamers:
-            if is_user_live(streamer):
-                update_data(streamer)
-        
-        time.sleep(5)
+            curr_info_json = curr_info(streamer)
+            print(curr_info_json)
+            if is_user_live(curr_info_json):
+                update_data(streamer, curr_info_json)
+        time.sleep(3)
 
 if __name__ == '__main__':
     main()
